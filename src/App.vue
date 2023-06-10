51f6b5af21386ce345cb37f18d4a31a117e698d5
William Tomé <william.tomenh@gmail.com>
2023-06-10 17:09:31 -0300

    feat: using props to send data from father component to child component.

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,61 @@
 <template>
-  <TheHeader>
-    <template v-slot:title>
-      <h1>Home</h1>
-    </template>
-    <template v-slot:description>
-      <p>Descrição</p>
-    </template>
-    Conteudo TRUE! Me dê papai!!!
-  </TheHeader>
+<!--  <TheHeader>-->
+<!--    <template v-slot:title>-->
+<!--      <h1>Home</h1>-->
+<!--    </template>-->
+<!--    <template v-slot:description>-->
+<!--      <p>Descrição</p>-->
+<!--    </template>-->
+<!--    Conteudo TRUE! Me dê papai!!!-->
+<!--  </TheHeader>-->
 
-  <BaseCard/>
+  <BaseAlert :variant="variant">
+    Dados cadastrados com sucesso.
+  </BaseAlert>
 
-  <h1>{{ fullName }}</h1>
+<!--  <BaseCard/>-->
 
-  <div v-if="accessLevel === 'admin'">
-    <p>Usuário Admin</p>
-  </div>
-  <div v-else-if="accessLevel === 'marketing'">
-    <p>Usuário Marketing</p>
-  </div>
-  <div v-else>
-    <p>Usuário Padrão</p>
-  </div>
+<!--  <h1>{{ fullName }}</h1>-->
 
-  <div>
-    <p>Profissão: <input type="text" name="profession" v-model="profession"></p>
-    <div>
-      Minha profissão é {{professionUpperCase}}
-    </div>
-  </div>
+<!--  <div v-if="accessLevel === 'admin'">-->
+<!--    <p>Usuário Admin</p>-->
+<!--  </div>-->
+<!--  <div v-else-if="accessLevel === 'marketing'">-->
+<!--    <p>Usuário Marketing</p>-->
+<!--  </div>-->
+<!--  <div v-else>-->
+<!--    <p>Usuário Padrão</p>-->
+<!--  </div>-->
 
-  <br><br>
+<!--  <div>-->
+<!--    <p>Profissão: <input type="text" name="profession" v-model="profession"></p>-->
+<!--    <div>-->
+<!--      Minha profissão é {{professionUpperCase}}-->
+<!--    </div>-->
+<!--  </div>-->
 
-  <TheFooter/>
+<!--  <br><br>-->
+
+<!--  <TheFooter/>-->
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue"
-import TheFooter from "./components/TheFooter.vue"
-import BaseCard from "./components/BaseCard.vue"
+// import TheHeader from "./components/TheHeader.vue"
+// import TheFooter from "./components/TheFooter.vue"
+// import BaseCard from "./components/BaseCard.vue"
+import BaseAlert from "./components/BaseAlert.vue"
 
 export default {
-  beforeCreate() {
-    console.log('beforeCreate')
-    console.log('Estado: ' + this.name)
-    console.log('DOM: ' + this.$el)
-  },
-  created() {
-    console.log('created')
-    console.log('Estado: ' + this.name)
-    console.log('DOM: ' + this.$el)
-  },
-  beforeMount() {
-    console.log('beforeMount')
-    console.log('Estado: ' + this.name)
-    console.log('DOM: ' + this.$el)
-  },
-  mounted() {
-    console.log('Mounted')
-    console.log('Estado: ' + this.name)
-    console.log('DOM: ' + this.$el)
-  },
   name: 'App',
   components: {
-    BaseCard,
-    TheHeader,
-    TheFooter
+    // BaseCard,
+    // TheHeader,
+    // TheFooter,
+    BaseAlert,
   },
   data() {
     return {
+      variant: 'success',
       user: {
         name: 'William',
         last_name: 'Tomé'
@@ -90,7 +77,27 @@ export default {
     },
   },
   methods: {
-  }
+  },
+  beforeCreate() {
+    console.log('beforeCreate')
+    console.log('Estado: ' + this.name)
+    console.log('DOM: ' + this.$el)
+  },
+  created() {
+    console.log('created')
+    console.log('Estado: ' + this.name)
+    console.log('DOM: ' + this.$el)
+  },
+  beforeMount() {
+    console.log('beforeMount')
+    console.log('Estado: ' + this.name)
+    console.log('DOM: ' + this.$el)
+  },
+  mounted() {
+    console.log('Mounted')
+    console.log('Estado: ' + this.name)
+    console.log('DOM: ' + this.$el)
+  },
 }
 </script>
 
---
+++ src/components/BaseAlert.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="baseClass">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseAlert",
+  props: {
+    variant: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+    }
+  },
+  computed: {
+    baseClass() {
+      return [
+          'alert',
+          this.variant ? `alert-${this.variant}` : ''
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.alert-success {
+  background-color: #42b983;
+  color: white;
+  font-weight: bold;
+  width: 500px;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 0 auto;
+}
+</style>
